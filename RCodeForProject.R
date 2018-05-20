7ace0d59b4845eb26e3478c0421dcde3f2b93be3
lumanskiy <lumansk@uchicago.edu>
2018-05-20 14:14:43 -0500

    made all of the values numbers and not characters, made a regression function

--- RCodeForProject.R
+++ RCodeForProject.R
@@ -1,9 +1,12 @@
 projectData <- read_csv("~/Desktop/Econometrics Project/DATA-to-import.csv")
+newProjectData <- projectData[0:159,]
 
-sizeOfGovernment <- projectData$`1  Size of Government`
-legalSystemPropertyRights <- projectData$`2  Legal System & Property Rights`
-soundMoney <- projectData$`3  Sound Money`
-freedomTrade <- (projectData$`4  Freedom to trade internationally` != is.na()
-regulation <- projectData$`5  Regulation`
-lGDP <- projectData$Lgdp
+sizeOfGovernment <- as.numeric(newProjectData$`1  Size of Government`)
+legalSystemPropertyRights <- as.numeric(newProjectData$`2  Legal System & Property Rights`)
+soundMoney <- as.numeric(newProjectData$`3  Sound Money`)
+freedomTrade <- as.numeric(newProjectData$`4  Freedom to trade internationally`)
+regulation <- as.numeric(newProjectData$`5  Regulation`)
+lGDP <- as.numeric(newProjectData$Lgdp)
+
+model1 <- lm(lGDP ~ sizeOfGovernment + legalSystemPropertyRights + soundMoney + freedomTrade + regulation, data=newProjectData)
 
