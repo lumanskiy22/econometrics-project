ca94ec8748404babcbb3b9bcd85d3cc364858e34
lumanskiy <lumansk@uchicago.edu>
2018-05-22 22:46:45 -0500

    i imported the new version of the data set so it changed a lot of things

--- RCodeForProject.R
+++ RCodeForProject.R
@@ -2,30 +2,25 @@ library(MASS)
 library(stargazer)
 
 
-projectData <- read_csv("~/Desktop/Econometrics Project/DATA-to-import.csv")
-newProjectData <- projectData[0:159,]
+library(readxl)
+newProjectData <- read_excel("~/Desktop/Econometrics Project/Final-Data.xlsx")
 
-sizeOfGovernment <- as.numeric(newProjectData$`1  Size of Government`)
-legalSystemPropertyRights <- as.numeric(newProjectData$`2  Legal System & Property Rights`)
-soundMoney <- as.numeric(newProjectData$`3  Sound Money`)
-freedomTrade <- as.numeric(newProjectData$`4  Freedom to trade internationally`)
-regulation <- as.numeric(newProjectData$`5  Regulation`)
-lGDP <- as.numeric(newProjectData$Lgdp)
 
-model1 <- lm(lGDP ~ sizeOfGovernment + legalSystemPropertyRights + soundMoney + freedomTrade + regulation, data=newProjectData)
+sizeOfGovernment <- as.numeric(newProjectData$gov_size)
+legalSystemPropertyRights <- as.numeric(newProjectData$legal)
+soundMoney <- as.numeric(newProjectData$money)
+freedomTrade <- as.numeric(newProjectData$trade_free)
+regulation <- as.numeric(newProjectData$reg)
+lGDP <- as.numeric(newProjectData$lgdp)
+unemploymentRates <- as.numeric(newProjectData$unemp)
+literacyRates <-as.numeric(newProjectData$lit)
+overallIndex <- as.numeric(newProjectData$summ_ind)
+
+##Regression Model for log(GDP) and overall index including literacy and unemployment rates
+model1 <- lm(lGDP ~ overallIndex + literacyRates + unemploymentRates, data = newProjectData)
 
 summary(model1)
-
-##Now, I want to make unemployment and literacy rates into decimals, not percentages
-#For Unemployment Rates
-vectorForUnemployment <- c(newProjectData$`Unemployment (%)`)
-unemploymentRates <- as.numeric(sub("%","",vectorForUnemployment))/100
-
-#For Literacy Rates
-vectorForLiteracy <- c(newProjectData$`Literacy Rates`)
-literacyRates <- as.numeric(sub("%","",vectorForLiteracy))/100
-
-##Regression Model including Literacy Rates and Unemployment Rates
+##Regression Model with all variables including Literacy Rates and Unemployment Rates
 
 model2 <- lm(lGDP ~ sizeOfGovernment + legalSystemPropertyRights + soundMoney + freedomTrade + regulation +unemploymentRates + literacyRates, data=newProjectData)
 
@@ -36,12 +31,14 @@ summary(model2)
 model3 <- rlm(lGDP ~ sizeOfGovernment + legalSystemPropertyRights + soundMoney + freedomTrade + regulation +unemploymentRates + literacyRates, data=newProjectData)
 summary(model3)
 
-## Do a regression model with just the economic freedom index, literacy rates, and unemployment
-
+##Regression Model with just size of government and legal system
+model4 <- lm(lGDP ~ sizeOfGovernment + legalSystemPropertyRights + literacyRates + unemploymentRates)
+summary(model4)
 ## Do subsamples: developing and undeveloped countries or just separate by continents 
 
 ##Code for summary statistic table output for Latex 
 stargazer(newProjectData)
 
-
+#Code for regression tables 
+stargazer(model1, model2, model3, model4)
 
